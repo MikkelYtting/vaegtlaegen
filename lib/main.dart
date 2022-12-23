355492b8cf676bee5974764c4a4a38320e8da36e
MikkelYtting <62931295+MikkelYtting@users.noreply.github.com>
2022-12-23 22:14:19 +0100

    Login struckture

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,14 @@
 import 'package:flutter/material.dart';
+import 'login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
-    );
-  }
+  runApp(
+    MaterialApp(
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        // Add other routes here
+      },
+    ),
+  );
 }
